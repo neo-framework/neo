49ffc84d54adcade836ec85b5a0a7c5355b5d8c7
YouniS Bensalah <younis.bensalah@gmail.com>
2016-07-18 15:16:31 +0200

    Add HelloController example

---
+++ src/MyApp/controllers/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Neo Framework
+ *
+ * @link https://neo-framework.github.io
+ * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ * @license MIT
+ */
+
+namespace MyApp;
+
+abstract class HelloController
+{
+
+    public function hello_action()
+    {
+        return '<h1>Hello</h1>';
+    }
+
+}
