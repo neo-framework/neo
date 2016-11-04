76f024cc0a37a781d45c8dad17baf54a31cabf6b
YouniS Bensalah <younis.bensalah@gmail.com>
2016-11-04 22:44:25 +0100

    Get rid of copyright in sample files

--- public/index.php
+++ public/index.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * Neo Framework
- *
- * @link https://neo-framework.github.io
- * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
- * @license MIT
- */
-
 session_start();
 
 define('ROOT_DIR', realpath(__DIR__ . '/..'));
