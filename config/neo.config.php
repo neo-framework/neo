e43ed1f28beebd84ae63c065c22aaab631a49435
YouniS Bensalah <younis.bensalah@gmail.com>
2018-10-24 22:23:34 +0200

    Add catch-exceptions option in config template

--- config/neo.config.php
+++ config/neo.config.php
@@ -4,9 +4,12 @@
     // Possible values: http://php.net/manual/en/timezones.php
     'timezone' => 'Europe/Berlin',
 
-    // In debug mode any errors/exceptions are printed.
+    // In debug mode any errors/exceptions are printed (if catch-exceptions = true).
     // You probably want to disable this in production.
     'debug' => true,
+
+    // Catch exceptions and die.
+    'catch-exceptions' => true,
 
     // Namespace of the application.
     // This is needed by factory classes to map a class name to the fully qualified name.
